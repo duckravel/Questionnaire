3fa62477f3577920a40ae0bafcee96d0b9f76645
duckravel <al396421@uji.es>
2020-09-07 12:50:52 +0800

    prototypev1

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$('#map1').css('height',$(window).height()-$('#menu').height()-30);
+$('#map1').css('width',$(window).width()-$('#data').width()-30);
+
+// const size = ()=>{
+//     $('#map1').css('height',$(window).height()-$('#menu').height()-30);
+//     $('#map1').css('width',$(window).width()-$('#data').width()-30);
+// }
+// size;
+// $(window).resize(size);
